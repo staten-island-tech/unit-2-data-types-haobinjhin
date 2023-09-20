118a5c973b10e4237dc5d805a2cb52787263f74e
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2023-09-20 13:22:39 -0400

    asdfasdf

--- nigerian.py
+++ nigerian.py
@@ -1,8 +1,9 @@
 
-
-
-number=input("number")
-if number==("1,3,5,7,9"):
-    print("odd")
-elif number==("2,4,6,8,0"):
-    print("even") 
+food = input("insert food: ")
+name = input("insert name: ")
+adj = input("insert adj: ")
+noun = input("insert noun: ")
+verb = input("insert verb: ")
+name2 = input("insert name2: ")
+print(f""" One day {name} was chomping on some banana, until {name2} appeared before him. Just then, {name} felt true fear and {verb} away. {name2} picked up 
+      his {noun} and threw it at {name}. {name} dodged the {noun} which made {name2} even {adj} """)
