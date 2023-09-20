0efa3b72de308cddcb3e6f6e472161755b505d18
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2023-09-20 14:00:39 -0400

    asdf

--- nigerian.py
+++ nigerian.py
@@ -1,9 +1,12 @@
 
-food = input("insert food: ")
-name = input("insert name: ")
-adj = input("insert adj: ")
-noun = input("insert noun: ")
-verb = input("insert verb: ")
-name2 = input("insert name2: ")
-print(f""" One day {name} was chomping on some banana, until {name2} appeared before him. Just then, {name} felt true fear and {verb} away. {name2} picked up 
-      his {noun} and threw it at {name}. {name} dodged the {noun} which made {name2} even {adj} """)
+def odd(number):
+    if number%2 == 0:
+       print("even")
+    else:
+        print("odd")
+odd()
+
+great = 
+def tip(service):
+    if service == great:
+       print('')
