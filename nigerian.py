70836978253887053b72655436a9e7eac26e24fe
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2023-09-21 13:59:07 -0400

    i did something

--- nigerian.py
+++ nigerian.py
@@ -4,9 +4,30 @@ def odd(number):
        print("even")
     else:
         print("odd")
-odd()
 
-great = 
-def tip(service):
-    if service == great:
-       print('')
+
+def factor(number, number2):
+    print(f"the factors of {number} and {number2} is: ")
+    for i in range (1, number + 1):
+        if number%i == 0:
+            print(i)
+    for i in range (1, number2 + 1):
+        if number%i == 0:
+            print(i)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
